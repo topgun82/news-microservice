60509418715e25ef8ff25a7785af2b9c7ed3dce0
Alex Stoica <dimitrie.stoica@canopy-cloud.com>
2015-07-22 13:19:03 +0100

    removed test for home

--- test/indexTest.js
+++ test/indexTest.js
@@ -2,17 +2,9 @@ var app     = require('../index.js');
 var request = require('supertest')(app);
 
 describe('/home api', function() {
-  it('should return hello world', function(done) {
+  it('should return information about the service', function(done) {
     request
       .get('/')
-      .expect('Hello world\n', done);
-  });
-});
-
-describe('/info api', function() {
-  it ('should return json', function(done) {
-   request
-     .get('/info')
-     .expect({}, done);
+      .expect({}, done);
   });
 });
